1c32536a77bf59d115dfd776a6416328a404bdc9
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-06-30 10:09:40 -0500

    ECMAscript 7

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+const numeros = [2, 3, 45, 3, 12, , 7, 8];
+const masNumeros = [3, 4, 7, 8, 9, 0, ...numeros];
+if (masNumeros.includes(12)) {
+    console.log("SI se encuentra xd")
+} else "NO se encuentra"
+
+
+// elevar a lap potencia
+let x = 2 // expnetes
+let y = 10 // base
+let potencia = y ** x
+console.log(potencia)
